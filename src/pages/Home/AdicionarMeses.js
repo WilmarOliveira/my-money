f4b2efe625e7fde99d60de13a704e54077d3d9ab
Wilmar Oliveira <wilmar_oliveira13@hotmail.com>
2020-09-24 20:19:40 -0300

    Implementação inicial do AdicionarMes

--- src/pages/Home/AdicionarMeses.js
+++ src/pages/Home/AdicionarMeses.js
@@ -1,20 +1,54 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
+import { Redirect } from 'react-router-dom';
+
+const minAno = 2019;
+const maxAno = 2022;
 
 const AdicionarMeses = () => {
+    const anos = [];
+    const meses = [];
+    const [redir, setRedir] = useState('');
+    const refAno = useRef('');
+    const refMes = useRef('');
+
+    for (let i = minAno; i <= maxAno; i++) {
+        anos.push(i);
+    }
+
+    for (let i = 1; i <= 12; i++) {
+        meses.push(i);
+    }
+
+    const zeroPad = (num) => {
+        if (num < 10) {
+            return '0' + num
+        } else {
+            return num;
+        }
+    }
+
+    const verMes = () => {
+        setRedir(`${refAno.current.value}-${refMes.current.value}`);
+    }
+
+    if (redir !== '') {
+        return <Redirect to={`/movimentacoes/${redir}`} />
+    }
 
     return(
         <>
             <h2>Adicionar Mês</h2>
-            <select>
-                <option value="2019" >2019</option>
-                <option value="2020" >2020</option>
+            <select ref={refAno} >
+                {anos.map((ano) => {
+                    return <option value={ano} >{ano}</option>
+                })}
             </select>
-
-            <select>
-                <option value="01" >01</option>
-                <option value="02" >02</option>
+            <select ref={refMes} >
+                {meses.map(zeroPad).map((mes) => {
+                    return <option value={mes} >{mes}</option>
+                })}
             </select>
-            <button>Adicionar Mês</button>
+            <button onClick={verMes} >Adicionar Mês</button>
         </>
     );
 }
